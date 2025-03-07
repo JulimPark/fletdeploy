92246a0ab8a78f7245d673a1d8bea30b249ca6bf
JulimPark <100160055+JulimPark@users.noreply.github.com>
2025-03-08 03:29:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main(page: ft.Page):
     page.bgcolor = 'blue'
     
     cols = ft.Text('cloudflare deploy',color='white')
-    cols = ft.Text('For Deploy test',color='white')
-    page.add(cols)
+    cols2 = ft.Text('For Deploy test',color='white')
+    page.add([cols,cols2])
     
 ft.app(target=main)
